6547975bfbdc3417f5dfc219ddb3364b51f6c29b
oriNuguri25 <byeongjun25@naver.com>
2025-11-14 20:17:41 +0900

    feat: 로그인 UI 완성

--- src/pages/login/ui/LoginPage.tsx
+++ src/pages/login/ui/LoginPage.tsx
@@ -1,13 +1,25 @@
+import { Logo } from "@/shared/assets";
 import KakaoLoginButton from "../components/KakaoLoginButton";
 
 const LoginPage = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
-      <div className="flex flex-col w-full space-between">
-        <div className="flex flex-col items-center justify-center">
-          <div className="text-2xl font-bold">포포</div>
+      <div className="flex flex-col w-full gap-12">
+        <div className="flex flex-col items-center justify-center gap-3">
+          <img
+            src={Logo}
+            alt="포포 로고"
+            className="w-[120px] h-[120px] object-contain"
+          />
+          <div className="flex text-headline-32-bold">포포</div>
+          <div className="flex text-16-medium text-center">
+            동네 사람들과 함께 산책을
+            <br />
+            즐겨보세요
+          </div>
         </div>
-        <div className="mt-10 px-0">
+
+        <div className="flex px-0">
           <KakaoLoginButton />
         </div>
       </div>
