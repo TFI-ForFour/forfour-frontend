4f907e6713ffcfe74f0bb7c603e7554219da9f6e
oriNuguri25 <byeongjun25@naver.com>
2025-11-21 14:15:36 +0900

    feat: 산책 생성 페이지 UI 1차 작업 완료

--- src/app/config/Router.tsx
+++ src/app/config/Router.tsx
@@ -1,9 +1,11 @@
 import MainLayout from "@/app/layout/MainLayout";
+import CreateWalkPage from "@/pages/createwalk/ui/CreateWalkPage";
 import LoginPage from "@/pages/login/ui/LoginPage";
 import MainPage from "@/pages/main/ui/MainPage";
 import MyPage from "@/pages/mypage/ui/MyPage";
 import MyWalksPage from "@/pages/walks/ui/MyWalksPage";
 import { createBrowserRouter } from "react-router-dom";
+import SubLayout from "../layout/SubLayout";
 
 export const router = createBrowserRouter([
   {
@@ -27,5 +29,15 @@ export const router = createBrowserRouter([
   {
     path: "/login",
     element: <LoginPage />,
+  },
+  {
+    path: "/",
+    element: <SubLayout />,
+    children: [
+      {
+        path: "createwalk",
+        element: <CreateWalkPage />,
+      },
+    ],
   },
 ]);
--- src/pages/main/components/CreateWalkButton.tsx
+++ src/pages/main/components/CreateWalkButton.tsx
@@ -1,6 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 const CreateWalkButton = () => {
+  const navigate = useNavigate();
   return (
-    <button className="flex w-full rounded-full py-4 items-center justify-center bg-sky-300">
+    <button
+      className="flex w-full rounded-full py-4 items-center justify-center bg-sky-300"
+      onClick={() => {
+        navigate("/createwalk");
+      }}
+    >
       <div className="text-white text-[18px] font-medium">
         지금 바로 산책 생성하기
       </div>
